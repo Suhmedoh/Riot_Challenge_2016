fb37ea18d64472da3e8a2dc5782ae66b1e329af6
Joshua Semedo <joshsemedo@gmail.com>
2016-05-04 22:20:24 -0400

    works on local machine running py script

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from RiotAPI import RiotAPI
 import json
+import operator
 
 def main():
     api = RiotAPI('e6e27f9f-d47d-4b3f-96d4-3aab2c2b8cee')
@@ -8,50 +9,90 @@ def main():
     champList = []
     second_dict = {}
     champIdizzle = 11
+    structured_data = {}
+    champion_name = {}
        
     # Get summoner data
-    response = api.get_summoner_by_name('KmancXC')
+    summoner_response = api.get_summoner_by_name('KmancXC')
     
     # Parse the output
-    temp = json.dumps(response)
+    temp = json.dumps(summoner_response)
     temp = json.loads(temp)
     summoner_id = (temp['kmancxc']['id'])
     summoner_name = (temp['kmancxc']['name'])
     
-    #For troubleshooting
-    print (summoner_id, summoner_name)
+    # For troubleshooting
+    #print (summoner_id, summoner_name)
     
     # Get all of a summoner's mastery data
     #response = api.get_summoner_mastery_data(summoner_id)
     #print (response)
     
     # Get a summoner's top X mastery data
-    response = api.get_top_mastery_data(summoner_id, champs)
+    mastery_response = api.get_top_mastery_data(summoner_id, champs)
     
     # Compile the stuff I care about into a dictionary
-    for x, val in enumerate(response):
+    for x, val in enumerate(mastery_response):
         data_dict[val['championId']] = val['championPoints']
     
     # Find out what lane the champion is being played in
     for key in data_dict:
         champList.append(key)
-    response = api.get_champion_role(summoner_id, champList)
-    #print (response)
+        #structured_data[summoner_id][key].append()
+    lane_response = api.get_champion_role(summoner_id, champList)
     
-    # Process the champion list data
-    #print (type(response['matches']))
-    for x, val in enumerate(response['matches']):
-        second_dict[val['champion']] = val['lane'], val['role']
+    # Figure out what roles the champion is being played in, and for how many games
+    for x, val in enumerate(lane_response['matches']):
+        key_test = val['champion']
+        if (val['lane'] == 'BOTTOM'):
+            role = val['role']
+        else:
+            role = val['lane']
+        if key_test not in second_dict:
+            second_dict[key_test] = {}
+            second_dict[key_test][role] = 0
+        else:
+            if role in second_dict[key_test]:
+                second_dict[key_test][role] += 1
+            else:
+                second_dict[key_test][role] = 0
+                
+    # Get percentage of games played in each role per champIdizzle
+    for id_key in second_dict:
+        role_games = 0
+        for role_key in second_dict[id_key]:
+            role_games += second_dict[id_key][role_key]
+        for role_key in second_dict[id_key]:
+            second_dict[id_key][role_key] /= role_games
+        #print (second_dict[id_key]['lane'])
+          
+    #combined_dict = {}
+    #for key in (data_dict.keys() | second_dict.keys()):
+    #    if key in data_dict: combined_dict.setdefault(key, []).append(data_dict[key])
+    #    if key in second_dict: combined_dict.setdefault(key, []).append(second_dict[key])
     
-    result = {}
-    for key in (data_dict.keys() | second_dict.keys()):
-        if key in data_dict: result.setdefault(key, []).append(data_dict[key])
-        if key in second_dict: result.setdefault(key, []).append(second_dict[key])
-    #print (result)
+    # Get the champion name for later use
+    for champIdizzle in data_dict:
+        champion_response = api.get_champion_name(champIdizzle)
+        champion_name[champIdizzle] = (champion_response['name'])
+        
+    # Put summoner id as a first level key
+    structured_data[summoner_id] = {}
     
-    # Get the champion name
-    response = api.get_champion_name(champIdizzle)
-    print (response)
-
+    # Put champion id as a second level key, with the name and another dictionary as the value
+    for champion_id in champion_name:
+        structured_data[summoner_id][champion_id] = [champion_name[champion_id]]
+        
+    # Put role as a third level key with mastery points * percentage of games in that role as the value
+    for second_champ_id, data_champ_id in zip(second_dict, data_dict):
+        for role_key in second_dict[second_champ_id]:
+            structured_data[summoner_id][second_champ_id].append({'role goes here': 
+                                                second_dict[second_champ_id][role_key] * data_dict[data_champ_id]})
+    
+    print (structured_data)
+    #print (data_dict)
+    #print ()
+    #print (second_dict)
+    
 if __name__ == '__main__':
     main()
---
+++ webpages/summoner_mastery_results.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <link href='https://fonts.googleapis.com/css?family=Gentium+Basic:400,700italic' rel='stylesheet' type='text/css'>
+  <link href='riot.css' rel='stylesheet' type='text/css'>
+  <title>DreamTeam.gg</title>
+	<script src="../scripts/jquery.js"></script>
+	<script src="../scripts/summoner_stuff.js"></script>
+</head>
+<body>
+	<div class="body_wrapper">
+		<h1>DreamTeam.gg</h1>
+
+		<h4><?php
+			$script = 'C:\xampp\htdocs\riot_challenge\Main.py';
+			$python = 'C:\Users\Josh\AppData\Local\Programs\Python\Python35\python.exe';
+			$command = escapeshellcmd($script);
+			$results = shell_exec($command);
+			echo $results; ?> </h4>
+			<div class="summoners">
+				<div id="summ1">
+					<span style="position: absolute; bottom: 0;">
+						<?php
+						echo $_POST['sum1'];
+						echo '<br>Top';
+						?>
+					</span>
+				</div><div id="summ2">
+					<span style="position: absolute; bottom: 0;">
+						<?php
+						echo $_POST['sum2'];
+						echo '<br>Mid';
+						?>
+					</span>
+				</div><div id="summ3">
+					<span style="position: absolute; bottom: 0;">
+						<?php
+						echo $_POST['sum3'];
+						echo '<br>Jungle';
+						?>
+					</span>
+				</div><div id="summ4">
+					<span style="position: absolute; bottom: 0;">
+						<?php
+						echo $_POST['sum4'];
+						echo '<br>Support';
+						?>
+					</span>
+				</div><div id="summ5">
+					<span style="position: absolute; bottom: 0;">
+						<?php
+						echo $_POST['sum5'];
+						echo '<br>ADC';
+						?>
+					</span>
+				</div>
+			</div>
+	</div>
+</body>
+</html>
